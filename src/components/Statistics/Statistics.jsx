21f17439f9ab6780e03d3d6ab15cb40a1732c387
Julija Faltina <dashinicha@gmail.com>
2021-08-18 10:45:52 +0300

    Add components maps

---
+++ src/components/Statistics/Statistics.jsx
@@ -0,0 +1,13 @@
+const Statistics = ({ good, neutral, bad, total, positiveFeedback}) => {
+    return (
+        <ul>
+            <li>
+                Good: <span>{ }</span>
+            </li>
+            <li><span></span></li>
+            <li><span></span></li>
+            <li><span></span></li>
+            <li><span></span></li>
+</ul>
+    )
+}
